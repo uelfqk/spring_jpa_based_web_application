9f7fcc6f68130a24be86cc9ba9bedf451f65d1bb
uelfqk <shhzg_zz@naver.com>
2021-02-20 22:28:41 +0900

    feat: 63. 모임 만들기

---
+++ src/main/java/com/studyolle/event/EventController.java
@@ -0,0 +1,34 @@
+package com.studyolle.event;
+
+import com.studyolle.account.CurrentUser;
+import com.studyolle.domain.Account;
+import com.studyolle.domain.Study;
+import com.studyolle.event.form.EventForm;
+import com.studyolle.study.StudyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/study/{path}")
+public class EventController {
+
+    private final StudyRepository studyRepository;
+
+    //TODO 2021.02.20 62. 모임 만들기
+    //                 1. 모임 만들기 폼에 필요한 데이터를 랜더링하여 클라이언트로 반환
+    @GetMapping("/new-event")
+    public String newEventForm(@CurrentUser Account account, @PathVariable String path, Model model) {
+        Study study = studyRepository.findByPath(path);
+
+        model.addAttribute("account", account);
+        model.addAttribute("study", study);
+        model.addAttribute("eventForm", new EventForm());
+
+        return "event/form";
+    }
+}
---
+++ src/main/java/com/studyolle/event/form/EventForm.java
@@ -0,0 +1,42 @@
+package com.studyolle.event.form;
+
+import com.studyolle.enums.EventType;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+//TODO 2021.02.20 62. 모임 만들기
+//                 1. 모임 만들기 뷰 랜더링을 위해 필요한 폼 데이터 클래스
+//                 2. 모임 모집 전략 -> 기본 선착순
+//                 3. 시간에 관련된 데이터를 폼으로 부터 받을 때의 전략 -> ISO DATE_TIME
+//                 4. 모임 최소 모집 인원 -> 2
+//                    기본값을 2로 설정
+
+@Getter @Setter
+public class EventForm {
+
+    @NotBlank
+    @Length(min = 50)
+    private String title;
+
+    private String description;
+    
+    private EventType eventType = EventType.FCFS;
+
+    @Min(2)
+    private Integer limitOfEnrollments = 2;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime endEnrollmentDateTime;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime startDateTime;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime endDateTime;
+}
